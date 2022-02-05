e9f2986f4607c4491670031d193eb2d5487ebe52
Kate Kohr <katekohr@icloud.com>
2022-02-04 21:14:48 -0500

    group project week 3/team 3

---
+++ exercise/src/main/java/com/techelevator/crm/Pet.java
@@ -0,0 +1,54 @@
+package com.techelevator.crm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pet {
+
+    private String name;
+    private String species;
+    private List<String> vaccinations = new ArrayList<>();
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public List<String> getVaccinations() {
+        return vaccinations;
+    }
+
+    public void setVaccinations(List<String> vaccinations) {
+        this.vaccinations = vaccinations;
+    }
+
+    public Pet(String name, String species)  {
+        this.name = name;
+        this.species = species;
+    }
+    public String listVaccinations() {
+        String ourVaccinations = "";
+        for(int i = 0; i < vaccinations.size(); i++) {
+          ourVaccinations += vaccinations.get(i);
+
+        }
+        return ourVaccinations;
+
+
+
+
+    }
+
+}
